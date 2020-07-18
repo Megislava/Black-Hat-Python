9ffeebf0309c3938ba91ba45a1e5fa0321e61724
Megislava <ajungmannova@gmail.com>
2020-07-18 17:45:06 -0400

    udp clinet

---
+++ network-basics/udp-client.py
@@ -0,0 +1,17 @@
+import socket
+
+target_host = "127.0.0.1"
+target_port = 80
+
+# create a socket object
+### AF_INET ~ IPv4
+### SOCK_DGRAM ~ UDP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# UDP -> connectionless -> no need to have connect function
+# send some data
+client.sendto(b"AAABBBCCC", (target_host, target_port))
+
+# receive some data
+data, addr = client.recvfrom(4096)
+
+print(data)
